a1a70bfe4cf7f04a41d597dc7454fbb5b78eacaa
tfirs <tfirst.dora@gmail.com>
2024-10-16 12:28:41 +0200

    init

---
+++ Test1.py
@@ -0,0 +1,11 @@
+print ("Test")
+
+#run : Shift + F10
+# OR: top-right arrow
+# OR: right click to Run 'script'
+
+# debugger
+    # break points: check the line's number -> the script will stop here
+
+for i in [1,2,3,4]:
+    print (i)
